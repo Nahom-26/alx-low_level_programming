0e17072f585c3ecca5bea020a022928813c6ba52
Nahom-26 <nteklewold@gmail.com>
2022-07-02 13:30:50 -0700

    pos or negg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,17 +9,17 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n < 0)
+if (n > 0)
 {
-	printf("%d is postive\n", n);
+printf("%d is postive\n", n);
 }
 else if (n == 0)
 {
-	printf("%d is zero\n", n);
+printf("%d is zero\n", n);
 }
-	else(n >0)
+else(n < 0)
 {
-	printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
