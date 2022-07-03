47b23c91648879d41e903b28723365dfbcb4c13f
Nahom-26 <nteklewold@gmail.com>
2022-07-03 02:31:04 -0700

    alphabets-2.0

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *  * main - entry point
+ *   * Return: always 0 (success)
+ **/
+int main(void)
+{
+	char c;
+	char C;
+
+	c = 'a';
+	C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar (c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar (C);
+		C++;
+	}
+	putchar ('\n');
+	return (0);
+}
