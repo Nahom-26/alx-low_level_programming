a0110cb0d0d1fc4430110f0da0ee16b3027217ed
Nahom-26 <nteklewold@gmail.com>
2022-07-06 10:29:55 -0700

    100-times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,60 +1,50 @@
 #include "main.h"
+
 /**
- *  print_times_table - prints timestable from n
- *   
- *  @n:input
- *     
- *  Return: void
- *     
- **/
+ * * print_times_table - times table function
+ * * @n: integer to return
+ *
+ * */
 void print_times_table(int n)
 {
-	int result;
+	int i;
+	int j;
 
-	if (n <= 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
-		int i;
-
 		for (i = 0; i <= n; i++)
 		{
-			int j;
-
 			for (j = 0; j <= n; j++)
 			{
-				result = i * j;
+				int prod = j * i;
+
 				if (j == 0)
 				{
-					_putchar('0' + result);
-				}
-				else if (result >= 10)
-				{
-					_putchar(' ');
-					_putchar('0' + (result / 10));
-					_putchar('0' + (result % 10));
-				}
-				else if (result >= 100)
-				{
-					_putchar(' ');
-					_putchar('0' + (result / 100));
-					_putchar('0' + ((result / 10) % 10));
-					_putchar('0' + (result % 10));
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				if (j != n)
+					_putchar('0');
+				} else if (prod <= 9)
 				{
 					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				} else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + ((prod / 10) % 10));
+					_putchar('0' + (prod % 10));
+				} else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
 				}
 			}
 			_putchar('\n');
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
