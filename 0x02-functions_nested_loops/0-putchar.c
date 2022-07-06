11cb961311a06716314945176f9a8a0c640571cb
Nahom-26 <nteklewold@gmail.com>
2022-07-06 05:08:02 -0700

    putcar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main.h":
 
 /**
  * main - prints _putchar
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putcar('_');
+	_putcar('p');
+	_putcar('u');
+	_putcar('t');
+	_putcar('c');
+	_putcar('h');
+	_putcar('a');
+	_putcar('r');
+	_putcar('\n');
 
 	return (0);
 
