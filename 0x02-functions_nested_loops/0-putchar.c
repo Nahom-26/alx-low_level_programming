2d20bd3edd25eefa4d7904ba213aac20f5c61588
Nahom-26 <nteklewold@gmail.com>
2022-07-06 04:40:17 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,21 @@
-#include <unistd.h>
+#include "main.h";
 
-      int _putchar(char c)
-	      {
-		      	return (write(1, &c, 1));
-				}
+/**
+ * main - prints _puchar
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
