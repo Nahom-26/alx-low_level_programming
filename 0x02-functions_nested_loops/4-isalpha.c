f0b7edb0f843d87f73fc5ad639a5328a17ec8e95
Nahom-26 <nteklewold@gmail.com>
2022-07-06 07:22:14 -0700

    is-alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,13 @@
 #include "main.h"
 /**
  *  *_isalpha - checks for alphabetic characters
+ *   *
+ *    *Description: checks for alpha
+ *     *
+ *      *Return: 1 - alphabetic character 0 - otherwise
+ *       *
+ *        *@c:input.
+ *         *
  **/
 int _isalpha(int c)
 {
