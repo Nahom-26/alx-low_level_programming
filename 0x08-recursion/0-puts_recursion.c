149aca0418cfbaadfddc34b5b0a8e29cfc31f0a9
Nahom-26 <nteklewold@gmail.com>
2022-07-20 11:56:38 -0700

    factorial

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/*********
+/*
  * _puts_recursion - prints a string followed by new line
  * @s: string to print
  * Return: nothing
  */
 
-void _puts_recursion(char *s)
+void _puts_recursion(char *s);
 {
 	int x = 0;
 
