40b7a86450ff1f10cf84fc1fffefbf831b84633d
Nahom-26 <nteklewold@gmail.com>
2022-07-20 12:13:45 -0700

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  *_strlen_recursion - returns str length
  * @s: string to calculate length
  * Return: strlen
@@ -8,9 +8,10 @@
 
 int _strlen_recursion(char *s)
 {
-	if (!*s)
+	if (*s == '\0')
 	{
 		return (0);
 	}
+	s++;
 	return (1 + _strlen_recursion(s));
 }
