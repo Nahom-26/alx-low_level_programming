1ca1a923844c2df2e4e915f1cda21791f93c22bd
Nahom-26 <nteklewold@gmail.com>
2022-07-22 13:13:14 -0700

    re

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Nahom-26
+ * Desc: Header file containing the prototypes of all functions
+ *      included in the static library libholberton.a.
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
